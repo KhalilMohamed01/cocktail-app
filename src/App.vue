e9ca75086aaaad427cb526ac78e701cfea108da5
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-06-01 14:59:46 +0200

    Integrating TheCocktailDB API and display random cocktails

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto">
+    <h1 class="text-4xl text-center mt-10 mb-5">Random Cocktails</h1>
+    <button @click="fetchCocktails" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5">Refresh Cocktails</button>
+    <div class="flex flex-wrap justify-center">
+      <CocktailCard v-for="(cocktail, index) in cocktails" :key="index" :cocktail="cocktail" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+import CocktailCard from './components/CocktailCard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CocktailCard
+  },
+  data() {
+    return {
+      cocktails: []
+    }
+  },
+  methods: {
+    async fetchCocktails() {
+      this.cocktails = [];
+      for (let i = 0; i < 3; i++) {
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+        const data = await response.json();
+        this.cocktails.push(data.drinks[0]);
+      }
+    }
+  },
+  mounted() {
+    this.fetchCocktails();
+  }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
