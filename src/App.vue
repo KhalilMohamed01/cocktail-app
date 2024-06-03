c4eb9d508ce5acea0500ffbdd9c68c0846193d01
KHALIL Mohamed <m3d.khalil01@gmail.com>
2024-06-03 11:32:13 +0200

    add animations on page load

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
-  <div class="h-[100vh]">
+  <div class="h-[100vh] relative">
     <div class="flex flex-wrap md:flex-nowrap h-full">
       <CocktailColumn
         v-for="(cocktail, index) in cocktails"
         :key="index"
         :cocktail="cocktail"
         :isExpanded="expandedColumn === index"
+        :index="index"
         @click="expandColumn(index)"
       />
     </div>
+    <button
+      @click="fetchCocktails"
+      class="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-yellow-900 cursor-pointer hover:bg-yellow-800 text-white p-4 rounded-full shadow-lg flex items-center"
+    >
+      <font-awesome-icon :icon="['fas', 'dice']" class="mr-2" />
+      Refresh Cocktails
+    </button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import CocktailColumn from './components/CocktailColumn.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDice } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faDice);
 
 export default {
   name: 'App',
   components: {
     CocktailColumn,
+    'font-awesome-icon': FontAwesomeIcon,
   },
   data() {
     return {
--- src/components/CocktailColumn.vue
+++ src/components/CocktailColumn.vue
@@ -2,19 +2,21 @@
   <div
     :class="[
       'flex flex-col transition-all duration-500',
+      'slide-in',
       isExpanded ? 'flex-grow w-full md:w-11/12' : 'items-center w-full md:w-1/12 cursor-pointer hover:bg-[#f1d49e81]',
       columnColor
     ]"
     @click="$emit('click')"
+    :style="{ animationDelay: `${index * 0.5}s` }"
   >
-    <div v-if="isExpanded" class=" mt-2 ml-4 text-[#989A8C] font-title">
-      <div class="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-16 font-title font-bold mr-4 mb-2" :style="{ fontSize: '4vw' }">
-        <span>{{ columnNumber }}</span>
+    <div v-if="isExpanded" class="mt-2 ml-4 text-[#989A8C]">
+      <div class="flex flex-col md:flex-row space-y-2 md:space-y-0 font-title font-bold mr-4 mb-2" :style="{ fontSize: '4vw' }">
+        <span class="text-center">{{ columnNumber }}</span>
       </div>
-      <div class="flex flex-col md:flex-row p-4 md:p-8 items-center md:h-full">
+      <div class="flex flex-col md:flex-row p-4 md:p-8 items-center">
         <img :src="cocktail.strDrinkThumb" alt="Cocktail Image" class="w-full md:w-1/3 aspect-square mb-2 rounded-lg" />
         <div class="pl-0 md:pl-8">
-          <span :style="{ fontSize: '5vw' }">{{ cocktail.strDrink }}</span>
+          <span class="font-title font-bold" :style="{ fontSize: '5vw' }">{{ cocktail.strDrink }}</span>
           <p class="mb-4">
             <font-awesome-icon :icon="['fas', 'champagne-glasses']" />
             &nbsp;&nbsp;<span>{{ cocktail.strCategory }}</span>
@@ -57,6 +59,7 @@ export default {
   props: {
     cocktail: Object,
     isExpanded: Boolean,
+    index: Number,
   },
   computed: {
     ingredients() {
@@ -71,13 +74,12 @@ export default {
       return ingredients;
     },
     columnNumber() {
-      return `0${this.$parent.cocktails.indexOf(this.cocktail) + 1}`;
+      return `0${this.index + 1}`;
     },
     columnColor() {
-      const index = this.$parent.cocktails.indexOf(this.cocktail);
       const colors = ['bg-color1', 'bg-color2', 'bg-color3'];
-      return colors[index % colors.length];
-    }
+      return colors[this.index % colors.length];
+    },
   },
 };
 </script>
@@ -95,11 +97,21 @@ export default {
 .bg-color3 {
   background-color: #F0ECE0;
 }
-
 .number-animation {
   transition: transform 0.3s ease-in-out;
 }
 .number-animation:hover {
   transform: translateY(-5px);
+}
+.slide-in {
+  opacity: 0;
+  transform: translateX(100%);
+  animation: slide-in 0.5s forwards;
+}
+@keyframes slide-in {
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
 }
 </style>
